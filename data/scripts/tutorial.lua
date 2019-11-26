7d216da147f8dfd12e355283dd9ab0e8bf1ec2c9
Naman Dixit <namandixit@iitb.ac.in>
2019-11-27 04:54:39 +0530

    Finished tutorial

--- data/scripts/tutorial.lua
+++ data/scripts/tutorial.lua
@@ -10,12 +10,54 @@ function tutorial.tutorial ()
          "            cd home"
    })
 
-   t.step("cd ..", {
+   t.step("cd ..", {"",
          "Now, move back one directory up by entering:",
+         "",
          "            cd .."
    })
 
-   t.finish({"Well Done!"})
+   t.step("ls", {"",
+      "To list the content of a directory, we use \"ls\" command.",
+      "Without any arguments, it list contents of current directory.",
+      "Enter the following command to continue:",
+      "",
+      "            ls"
+})
+
+t.step("ls tmp", {"",
+      "Giving the name of a directory lists its contents, like below:",
+      "",
+      "            ls tmp"
+})
+
+t.step("mkdir dir", {"",
+      "\"mkdir\" is used to create new directories.",
+      "For example, enter the following command:",
+      "",
+      "            mkdir dir"
+})
+
+t.step("touch file", {"",
+      "\"touch\" is used to create new empty files.",
+      "To create a new file in the current directory, enter:",
+      "",
+      "            touch file"
+})
+
+t.step("mv file dir", {"",
+      "\"mv\" is used to move files and directories.",
+      "Let's move the newly created file into the newly created directory:",
+      "",
+      "            mv file dir"
+})
+
+t.step("ls dir", {"",
+      "To verify that the move worked, let's list the contents of the directory",
+      "",
+      "            ls dir"
+})
+
+   t.finish({"", "Tutorial complete!", "Well Done!"})
 end
 
 return tutorial
